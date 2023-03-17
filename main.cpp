2888bed149bc346549c3aea3576ad138927faf97
Romulo Balsalobre <romulobalsalobre@gmail.com.br>
2023-03-17 20:02:37 -0300

    Calculadora de soma

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//using namespace std;
+
+int main(void)
+{
+    int a,b,soma;
+    std::cout << "Digite o primeiro numero:\n";
+    std::cin >> a;
+    std::cout << "Digite o segundo numero:\n";
+    std::cin >> b;
+    soma = a+b;
+    std::cout <<"O resultado da soma é: " << soma << "\n";
+}
